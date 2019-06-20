2e0b17d3cab26b8fe7120f0907da6668f0d34971
LiuHualin <liaochenlanruo@webmail.hzau.edu.cn>
2019-06-20 20:47:18 +0800

    add if else to draw different heatmap

--- Functions/ANI/Plot_ANIheatmap.R
+++ Functions/ANI/Plot_ANIheatmap.R
@@ -4,6 +4,10 @@ require(pheatmap)
 pdf("ANI_matrix.pdf")
 
 ani <- read.delim("ANIs.heatmap", sep="\t", row.names=1, header=T, stringsAsFactors=FALSE, check.names=FALSE)
-pheatmap(ani, border_color = "white" ,fontsize_row = 3, fontsize_col = 3)
+if (any( ani == 0)){
+	pheatmap(ani, border_color = "white" ,fontsize_row = 3, fontsize_col = 3, color = c(colorRampPalette(colors = c("grey"))(100), colorRampPalette(colors = c("yellow","red"))(50)), legend_breaks = c(0, 75, 83, 90, 100), legend_labels = c("< 75", "75", "83", "90", "100"))
+}else{
+	pheatmap(ani, border_color = "white" ,fontsize_row = 3, fontsize_col = 3)
+}
 
 dev.off()
