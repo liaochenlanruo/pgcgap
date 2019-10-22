499445782a7b3ff3a4ca79523204faccf20544c2
LiuHualin <liaochenlanruo@webmail.hzau.edu.cn>
2019-10-22 14:58:21 +0800

    Fixed a bug of plot_3Dpie.R, Optimized image display, and a fan chart has been added

--- Functions/Pan/plot_3Dpie.R
+++ Functions/Pan/plot_3Dpie.R
@@ -9,10 +9,14 @@ library("plotrix")
 pdf("Pangenome_Pie.pdf")
 stat = read.table("summary_statistics.txt", sep="\t")
 
-type <- stat[1:4, 1]
-def <- stat[1:4, 2]
-num <- stat[1:4, 3]
-total <- stat[5, 3]
+stats <- stat[rowSums(stat==0)==0,]
+row <- nrow(stats)
+row2 <- row-1
+
+type <- stats[1:row2, 1]
+def <- stats[1:row2, 2]
+num <- stats[1:row2, 3]
+total <- stats[row, 3]
 per <- round(num/sum(num)*100,2)
 percent <- paste(per, "%", sep="")
 
@@ -20,17 +24,7 @@ label <- paste(num,"(",percent,")", sep=" ")
 labels <- paste(type, "\n", label)
 
 
-i=1;
-while(i<=4){
-    if(num[i] <= 0){
-        num[i] = 0.00001
-    }else{
-        num[i] = num[i]
-    }
-    i=i+1
-}
-
-
 par(oma=c(2,2,2,2))
-pie3D(num, radius=0.75, height=0.15, labels=labels, explode=0.25, border="black", shade=0.6, cex.main=2, labelcex=1")
+pie3D(num, radius=0.75, height=0.1, labels=labels, explode=0, border="black", shade=0.6, cex.main=2, labelcex=0.9)
+fan.plot(num, labels=labels)
 dev.off()
