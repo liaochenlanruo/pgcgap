b420b78767d95ac3e2e3f2ef4e7a256594f81357
LiuHualin <liaochenlanruo@webmail.hzau.edu.cn>
2019-10-23 09:29:29 +0800

    Change the font size

--- Functions/ANI/Plot_ANIheatmap.R
+++ Functions/ANI/Plot_ANIheatmap.R
@@ -9,9 +9,9 @@ order_row = aa$tree_row$order#get the row order
 datat = data.frame(ani[order_row,order_row],check.names =F) #rearrange the original datas according to the new row order
 
 if (any( ani == 0)){
-	pheatmap(datat, border_color = "white" ,fontsize_row = 3, fontsize_col = 3, color = c(colorRampPalette(colors = c("grey"))(100), colorRampPalette(colors = c("yellow","red"))(50)), legend_breaks = c(0, 75, 83, 90, 100), legend_labels = c("< 75", "75", "83", "90", "100"), cluster_rows = TRUE, cluster_cols = FALSE)
+	pheatmap(datat, border_color = "white" ,fontsize_row = 5, fontsize_col = 5, color = c(colorRampPalette(colors = c("grey"))(100), colorRampPalette(colors = c("yellow","red"))(50)), legend_breaks = c(0, 75, 83, 90, 100), legend_labels = c("< 75", "75", "83", "90", "100"), cluster_rows = TRUE, cluster_cols = FALSE)
 }else{
-	pheatmap(datat, border_color = "white" ,fontsize_row = 3, fontsize_col = 3, cluster_rows = TRUE, cluster_cols = FALSE)
+	pheatmap(datat, border_color = "white" ,fontsize_row = 5, fontsize_col = 5, cluster_rows = TRUE, cluster_cols = FALSE)
 }
 
 dev.off()
