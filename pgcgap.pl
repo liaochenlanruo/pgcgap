26a2319ebc19fb271a7acded67d32266f43845ae
liaochenlanruo <liaochenlanruo@webmail.hzau.edu.cn>
2021-06-28 16:50:14 +0800

    Update pgcgap.pl

--- pgcgap.pl
+++ pgcgap.pl
@@ -1417,7 +1417,7 @@ if ($opt_All or $opt_Assemble or $opt_Annotate or $opt_CoreTree or $opt_Pan or $
 GetOptions(%options) or pod2usage("Try '$0 --help' for more information.");
 
 if($opt_version){
-	print "PGCGAP version: 1.0.29\n";
+	print "PGCGAP version: 1.0.30\n";
 	print "Enter the command 'pgcgap --check-update' to check if there is a new version, and update to the new version if it exists.\n";
 	exit 0;
 }
